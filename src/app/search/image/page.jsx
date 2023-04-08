4de47e2f7cbd6ecc5215752082aff23ca8a7dc38
Ayush_sccg <ayush1240945@gmail.com>
2023-04-08 10:28:32 +0545

    Create search page for web and image

---
+++ src/app/search/image/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ImageSearchPage() {
+  return (
+    <div>ImageSearchPage</div>
+  )
+}
---
+++ src/app/search/layout.jsx
@@ -0,0 +1,15 @@
+
+import SearchHeader from '@/components/SearchHeader'
+import './../globals.css'
+
+
+export default function SearchLayout({ children }) {
+  return (
+      
+      <div>
+        <SearchHeader/>
+        {children}
+        {/* Footer */}
+      </div>
+  )
+}
