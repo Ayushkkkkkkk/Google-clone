83dcbed35dd468a4cfcdc54c8cdb7d34a64fb40d
Ayush_sccg <ayush1240945@gmail.com>
2023-04-08 09:38:58 +0545

    Add body section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,27 @@
+import HomeHeader from '@/components/HomeHeader'
+import HomeSearch from '@/components/HomeSearch'
+import Image from 'next/image'
 import React from 'react'
 
-export default function Home() {
+const Home = () => {
   return (
-    <div className='text-red-900'>Home</div>
+    <>
+    {/* Header */}
+      <HomeHeader/>
+
+      {/* body */}
+      <div className="flex flex-col items-center mt-24">
+        <Image
+        width="300"
+        height="100"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/800px-Google_2015_logo.svg.png"
+
+        />
+        <HomeSearch/>
+      </div>
+    </>
+    
   )
 }
+
+export default Home
