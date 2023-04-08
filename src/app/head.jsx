d6c1d94727f379bd45e3c4be7fa309f0d7226212
Ayush_sccg <ayush1240945@gmail.com>
2023-04-08 12:41:54 +0545

    update the search header component

--- src/app/head.jsx
+++ src/app/head.jsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>Google clone</title>
+      <title>Google</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Google clone created by next js" />
       <link rel="icon" href="/favicon.svg" />
--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import SearchBox from "./SearchBox";
+import {FcSettings} from "react-icons/fc"
+import {TbGridDots} from "react-icons/tb"
+import SearchHeaderOptions from "./SearchHeaderOptions";
 
 export default function SearchHeader() {
   return (
-    <div>SearchHeader</div>
-  )
+    <header className="sticky top-0 bg-white ">
+      <div className="flex w-full p-6 items-center justify-between ">
+        <Link href={"/"}>
+          <Image
+            width="120"
+            height="40"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/800px-Google_2015_logo.svg.png"
+          />
+        </Link>
+        <div className="flex-1">
+          <SearchBox/>
+        </div>
+        <div className="hidden md:inline-flex space-x-2 ">
+         <FcSettings className="header-icon"/>
+         <TbGridDots className="header-icon"/>
+        </div>
+        <button className="bg-blue-500 text-white px-6 py-2 font-medim rounded-md hover:brightness-105 hover:shadow-md transtition-all ml-2">Sign in</button>
+      </div>
+      <SearchHeaderOptions/>
+    </header>
+  );
 }
